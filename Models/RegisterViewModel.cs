a686f6211868bf4f059f2536e108ffa129979468
Mistura Raheem <olawunmiraheem@gmail.com>
2025-03-27 02:24:41 -0400

    layout creation

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentEasy.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
